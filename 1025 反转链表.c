bb8cd4bd55eae6114b13cb8eed158c8465f058ac
ChangAkira <a259759666@163.com>
2026-02-21 22:32:30 +0800

    1

--- 1025 反转链表.c
+++ 1025 反转链表.c
@@ -7,13 +7,56 @@
 // 好奇怪的题啊，是我们可以直接操控数据存在哪个虚拟地址中吗？没听说过这种用法啊？
 // 估计是扔大数组了
 
-#include<stdio.h>
+#include <stdio.h>
+struct L {
+    int data;
+    int next;
+} List[100000];
 
-int main(){
-    struct L{
-        int address;
-        int data;
-        int next;
-    }List[100000];
+int arr[100000] = {0};
+
+void arrswap(int arr[], int i, int swapcnt) {
+    int left = i - swapcnt + 1;
+    int right = i;
+    int temp;
+    while (left <= right) {
+        temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        right--;
+        left++;
+    }
+}
+
+int main() {
+    int base, cnt, swapcnt;
+    scanf("%d%d%d", &base, &cnt, &swapcnt);
+    int a = 0, b = 0, c = 0;
+    for (int i = 0; i < cnt; i++) {
+        scanf("%d%d%d", &a, &b, &c);
+        List[a].data = b;
+        List[a].next = c;
+    }
+
+    int cnt2 = 0;
+    for (int i = base; i != -1; i = List[i].next, cnt2++) {
+        arr[cnt2] = i;
+    }
+    for (int i = 0, k = 1; i < cnt2; i++, k++) {
+        if (k == swapcnt) {
+            arrswap(arr, i, swapcnt);
+            k = 0;
+        }
+    }
+    for (int i = 0; i < cnt2; i++) {
+
+        if (i != cnt2 - 1) {
+            printf("%05d %d %05d", arr[i], List[arr[i]].data, arr[i + 1]);
+
+        } else {
+            printf("%05d %d -1", arr[i], List[arr[i]].data);
+        }
+        printf("\n");
+    }
     return 0;
 }
