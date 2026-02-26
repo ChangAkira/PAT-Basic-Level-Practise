c90f67c84c9428120922563611a91be831c05626
ChangAkira <a259759666@163.com>
2026-02-26 11:15:21 +0800

    汗流浃背了

--- 1034 有理数四则运算.c
+++ 1034 有理数四则运算.c
@@ -1,4 +1,5 @@
 // 1034 有理数四则运算
+// 这道题真的汗流浃背了
 
 #define bool int
 #define true 1
@@ -25,33 +26,34 @@ long long gcd(long long a, long long b) { // 求最大公约数
     }
 }
 
-void simplifyd(rationalNumbers *number) {
-    long long gcdNum = gcd(number->partB, number->partC);
-    number->partB /= gcdNum;
-    number->partC /= gcdNum;
-    long long addPartA = number->partB / number->partC;
-    number->partB = number->partB % number->partC;
-    number->partA += addPartA;
-    return;
+rationalNumbers simplifyd(rationalNumbers number) { // 不化简直接计算，计算时带符号算。输出前化简，化简包括取绝对值，输出符号由标志位控制
+    number.partA = llabs(number.partA);
+    number.partB = llabs(number.partB);
+    number.partC = llabs(number.partC);
+    long long gcdNum = gcd(number.partB, number.partC);
+    number.partB /= gcdNum;
+    number.partC /= gcdNum;
+    long long addPartA = number.partB / number.partC;
+    number.partB = number.partB % number.partC;
+    number.partA += addPartA;
+    if (number.partA != 0 && number.partB != 0) {
+        number.fullFlag = true;
+    }
+    return number;
 }
 
 void inputNumber(rationalNumbers *number) {
-    // 先用 * 解引用拿到实体，然后直接塞给它一个全为 0 的匿名结构体
-    *number = (rationalNumbers){0};
     number->partA = 0;
     scanf("%lld/%lld", &number->partB, &number->partC);
     if (number->partB < 0) {
-        number->partB = llabs(number->partB);
+        // number->partB = llabs(number->partB);
         number->negFlag = true;
-    }
-    simplifyd(number);
-    if (number->partA != 0 && number->partB != 0) {
-        number->fullFlag = true;
     }
     return;
 }
 
 void outputNumber(rationalNumbers number) {
+    number = simplifyd(number);
     if (number.partA == 0 && number.partB == 0) {
         printf("0");
         return;
@@ -74,7 +76,83 @@ void outputNumber(rationalNumbers number) {
     return;
 }
 
-rationalNumbers addition(rationalNumbers *number1, rationalNumbers *number2) {
-    rationalNumbers result;
-    result.partA =
+void addition(rationalNumbers number1, rationalNumbers number2) {
+    rationalNumbers result = {false, false, 0, 0, 0};
+    result.partC = number1.partC * number2.partC;
+    result.partB = number1.partB * number2.partC + number2.partB * number1.partC;
+    if (result.partB < 0) {
+        result.negFlag = true;
+    }
+    outputNumber(number1);
+    printf(" + ");
+    outputNumber(number2);
+    printf(" = ");
+    outputNumber(result);
+    return;
+}
+
+void subtraction(rationalNumbers number1, rationalNumbers number2) {
+    rationalNumbers result = {false, false, 0, 0, 0};
+    result.partC = number1.partC * number2.partC;
+    result.partB = number1.partB * number2.partC - number2.partB * number1.partC;
+    if (result.partB < 0) {
+        result.negFlag = true;
+    }
+    outputNumber(number1);
+    printf(" - ");
+    outputNumber(number2);
+    printf(" = ");
+    outputNumber(result);
+    return;
+}
+void multiplication(rationalNumbers number1, rationalNumbers number2) {
+    rationalNumbers result = {false, false, 0, 0, 0};
+    result.partC = number1.partC * number2.partC;
+    result.partB = number1.partB * number2.partB;
+    if (result.partB < 0) {
+        result.negFlag = true;
+    }
+    outputNumber(number1);
+    printf(" * ");
+    outputNumber(number2);
+    printf(" = ");
+    outputNumber(result);
+    return;
+}
+void division(rationalNumbers number1, rationalNumbers number2) {
+    if (number2.partA == 0 && number2.partB == 0) {
+        outputNumber(number1);
+        printf(" / ");
+        outputNumber(number2);
+        printf(" = Inf");
+        return;
+    }
+    rationalNumbers result = {false, false, 0, 0, 0};
+    result.partC = number1.partC * number2.partB;
+    result.partB = number1.partB * number2.partC;
+    if ((result.partC < 0 && result.partB > 0) || (result.partC > 0 && result.partB < 0)) {
+        result.negFlag = true;
+    }
+    outputNumber(number1);
+    printf(" / ");
+    outputNumber(number2);
+    printf(" = ");
+    outputNumber(result);
+    return;
+}
+
+int main() {
+    rationalNumbers number1 = {false, false, 0, 0, 0};
+    rationalNumbers number2 = {false, false, 0, 0, 0};
+    inputNumber(&number1);
+    inputNumber(&number2);
+    addition(number1, number2);
+    printf("\n");
+    subtraction(number1, number2);
+    printf("\n");
+    multiplication(number1, number2);
+    printf("\n");
+    division(number1, number2);
+
+    return 0;
 }
