478f0a44374e462abd7cb9409eaa3d511ba8a8a1
ChangAkira <a259759666@163.com>
2026-03-09 12:15:18 +0800

    1

--- 1036 跟奥巴马一起编程.c
+++ 1036 跟奥巴马一起编程.c
@@ -1,2 +1,31 @@
-//1036 跟奥巴马一起编程
-#include<stdio.h>
+// 1036 跟奥巴马一起编程
+#include <math.h>
+#include <stdio.h>
+int main() {
+    int cnt;
+    char c;
+    scanf("%d %c", &cnt, &c);
+    int cnt_2 = round(cnt / 2.0);
+    if (cnt == 1) {
+        printf("%c", c);
+        return 0;
+    }
+    if (cnt == 0) {
+        return 0;
+    }
+    for (int i = 0; i < cnt; i++) {
+        printf("%c", c);
+    }
+    printf("\n");
+    for (int i = 0; i < cnt_2 - 2; i++) {
+        printf("%c", c);
+        for (int j = 0; j < cnt - 2; j++) {
+            printf(" ");
+        }
+        printf("%c\n", c);
+    }
+    for (int i = 0; i < cnt; i++) {
+        printf("%c", c);
+    }
+    return 0;
+}
